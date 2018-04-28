9752719b728e6ef8679c70e37f272a9cabff9409
chikuwa111 <take0shun1111@gmail.com>
2018-04-29 00:59:53 +0900

    Move index.html and fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: '.'
   },
   module: {
     rules: [
